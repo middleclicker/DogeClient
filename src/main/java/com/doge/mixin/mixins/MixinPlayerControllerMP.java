b8db78ea4e15bf98cf941fad259e2b10078d22fa
middleclicker <60602265+middleclicker@users.noreply.github.com>
2021-10-09 17:34:55 +0800

    Add files via upload

---
+++ src/main/java/com/doge/mixin/mixins/MixinPlayerControllerMP.java
@@ -0,0 +1,10 @@
+package com.doge.mixin.mixins;
+
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(PlayerControllerMP.class)
+public class MixinPlayerControllerMP {
+    // TODO: Make reach module
+}
