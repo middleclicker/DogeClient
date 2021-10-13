91d1edbe24f62e2bf8a8c2d295c55cc563cddb4a
middleclicker <60602265+middleclicker@users.noreply.github.com>
2021-10-13 11:20:03 +0800

    Delete src/main directory

--- src/main/java/com/doge/api/event/events/PlayerUpdateEvent.java
+++
@@ -1,9 +0,0 @@
-package com.doge.api.event.events;
-
-import com.doge.api.event.Event;
-
-public class PlayerUpdateEvent extends Event {
-    public PlayerUpdateEvent() {
-        super();
-    }
-}
--- src/main/java/com/doge/api/setting/Setting.java
+++
@@ -1,9 +0,0 @@
-package com.doge.api.setting;
-
-import com.doge.client.module.Module;
-
-public class Setting {
-    public String name;
-    public Module parent;
-    public Type type;
-}
