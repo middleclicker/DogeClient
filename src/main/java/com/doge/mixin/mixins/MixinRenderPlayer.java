d95b93eaa71e27eea418732a06a89282dfa159da
middleclicker <60602265+middleclicker@users.noreply.github.com>
2021-10-19 20:05:07 +0800

    Delete src/main directory

--- src/main/java/com/doge/mixin/mixins/MixinRenderPlayer.java
+++
@@ -1,9 +0,0 @@
-package com.doge.mixin.mixins;
-
-import net.minecraft.client.renderer.entity.RenderPlayer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(RenderPlayer.class)
-public class MixinRenderPlayer {
-
-}
